a3707efb8fb9b5caf5d9b13d8ba4d37efce47a8f
michaelwenk <michael.wenk@uni-jena.de>
2021-04-09 14:18:22 +0200

    feat: added link to NMRShiftDB in ResultCardText

--- src/component/panels/resultsPanel/resultsContainer/resultCard/ResultCardText.jsx
+++ src/component/panels/resultsPanel/resultsContainer/resultCard/ResultCardText.jsx
@@ -2,17 +2,31 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 /** @jsxImportSource @emotion/react */
 import Card from 'react-bootstrap/Card';
+import { FaExternalLinkAlt } from 'react-icons/fa';
 
 function ResultCardText({ molecule }) {
   return (
-    <Card.Text>
+    <Card.Text style={{ fontSize: '14px' }}>
       Formula: {molecule.meta.mf}
       <br />
       RMSD: {molecule.meta.rmsd ? Number(molecule.meta.rmsd).toFixed(3) : ''}
       <br />
-      Tanimoto: {molecule.meta.tanimoto ? Number(molecule.meta.tanimoto) : ''}
+      Tanimoto:{' '}
+      {molecule.meta.tanimoto ? Number(molecule.meta.tanimoto).toFixed(5) : ''}
       {molecule.meta.id ? <br /> : null}
-      {molecule.meta.id ? 'NMRShiftDB ID: ' + molecule.meta.id : ''}
+      {molecule.meta.id ? (
+        <a
+          href={`http://www.nmrshiftdb.org/molecule/${molecule.meta.id}`}
+          target="_blank"
+          rel="noreferrer"
+          title={`Link to molecule ${molecule.meta.id} in NMRShiftDB`}
+        >
+          <FaExternalLinkAlt size="11" />
+          {` ${molecule.meta.id}`}
+        </a>
+      ) : (
+        ''
+      )}
     </Card.Text>
   );
 }
