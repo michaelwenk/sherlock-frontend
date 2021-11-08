831d4ac9fb7cca50230c36c1ac9753ef73b3eded
michaelwenk <michael.wenk@uni-jena.de>
2021-11-08 18:41:01 +0100

    feat: added search input field in retrievel tab

--- src/component/panels/queryPanel/tabs/QueryTabRetrieval.tsx
+++ src/component/panels/queryPanel/tabs/QueryTabRetrieval.tsx
@@ -12,117 +12,140 @@ import OCLnmr from 'react-ocl-nmr';
 import OCL from 'openchemlib/full';
 import { Molecule } from 'openchemlib';
 import CustomModal from '../../../elements/Modal';
+import Input from '../../../elements/Input';
 
 function QueryTabRetrieval() {
   const { resultDataDB } = useData();
   const { setFieldValue, submitForm } = useFormikContext<QueryOptions>();
   const [showDeleteAllModal, setShowDeleteAllModal] = useState<boolean>(false);
+  const [searchPattern, setSearchPattern] = useState<string>('');
 
-  const rows = useMemo(() => {
+  const filteredResultDataDB = useMemo(() => {
     return resultDataDB
-      ? resultDataDB.map((resultRecord) => {
-          const date = new Date(resultRecord.date);
-          const molecule = Molecule.fromSmiles(
-            resultRecord.previewDataSet.meta.smiles,
-          );
-
-          return (
-            <tr key={`resultDataDB_${resultRecord.id}`}>
-              <td>{resultRecord.name}</td>
-              <td>{`${date.getFullYear()}-${date.getMonth()}-${date.getDay()}, ${date.getHours()}:${date.getMinutes()}`}</td>
-              <td>{resultRecord.dataSetListSize}</td>
-              <td>
-                <OCLnmr
-                  OCL={OCL}
-                  id={`molSVG${resultRecord.id}`}
-                  width={100}
-                  height={100}
-                  molfile={molecule.toMolfileV3()}
-                  setSelectedAtom={() => {}}
-                  atomHighlightColor={'red'}
-                  atomHighlightOpacity={0.35}
-                  highlights={[]}
-                  setHoverAtom={() => {}}
-                  setMolfile={() => {}}
-                />
-              </td>
-              <td style={{ borderRight: 'none' }}>
-                <Button
-                  child={<FaEye title="Load from Database" />}
-                  onClick={() => {
-                    setFieldValue('queryType', queryTypes.retrieval);
-                    setFieldValue(
-                      'retrievalOptions.action',
-                      retrievalActions.retrieve,
-                    );
-                    setFieldValue('retrievalOptions.resultID', resultRecord.id);
-                    submitForm();
-                  }}
-                />
-              </td>
-              <td>
-                <Button
-                  child={<FaTrashAlt title="Delete in Database" />}
-                  onClick={() => {
-                    setFieldValue('queryType', queryTypes.retrieval);
-                    setFieldValue(
-                      'retrievalOptions.action',
-                      retrievalActions.deletion,
-                    );
-                    setFieldValue('retrievalOptions.resultID', resultRecord.id);
-                    submitForm();
-                  }}
-                />
-              </td>
-            </tr>
-          );
-        })
+      ? resultDataDB.filter((resultRecord) =>
+          resultRecord.name?.toLowerCase().includes(searchPattern),
+        )
       : [];
-  }, [resultDataDB, setFieldValue, submitForm]);
+  }, [resultDataDB, searchPattern]);
+
+  const filteredRows = useMemo(
+    () =>
+      filteredResultDataDB.map((resultRecord) => {
+        const date = new Date(resultRecord.date);
+        const molecule = Molecule.fromSmiles(
+          resultRecord.previewDataSet.meta.smiles,
+        );
+
+        return (
+          <tr key={`resultDataDB_${resultRecord.id}`}>
+            <td>{resultRecord.name}</td>
+            <td>{`${date.getFullYear()}-${date.getMonth()}-${date.getDay()}, ${date.getHours()}:${date.getMinutes()}`}</td>
+            <td>{resultRecord.dataSetListSize}</td>
+            <td>
+              <OCLnmr
+                OCL={OCL}
+                id={`molSVG${resultRecord.id}`}
+                width={100}
+                height={100}
+                molfile={molecule.toMolfileV3()}
+                setSelectedAtom={() => {}}
+                atomHighlightColor={'red'}
+                atomHighlightOpacity={0.35}
+                highlights={[]}
+                setHoverAtom={() => {}}
+                setMolfile={() => {}}
+              />
+            </td>
+            <td style={{ borderRight: 'none' }}>
+              <Button
+                child={<FaEye title="Load from Database" />}
+                onClick={() => {
+                  setFieldValue('queryType', queryTypes.retrieval);
+                  setFieldValue(
+                    'retrievalOptions.action',
+                    retrievalActions.retrieve,
+                  );
+                  setFieldValue('retrievalOptions.resultID', resultRecord.id);
+                  submitForm();
+                }}
+              />
+            </td>
+            <td>
+              <Button
+                child={<FaTrashAlt title="Delete in Database" />}
+                onClick={() => {
+                  setFieldValue('queryType', queryTypes.retrieval);
+                  setFieldValue(
+                    'retrievalOptions.action',
+                    retrievalActions.deletion,
+                  );
+                  setFieldValue('retrievalOptions.resultID', resultRecord.id);
+                  submitForm();
+                }}
+              />
+            </td>
+          </tr>
+        );
+      }),
+    [filteredResultDataDB, setFieldValue, submitForm],
+  );
 
   return (
     <div className="query-tab-retrieval-container">
-      <div className="button-container">
-        <Button
-          child={<FaSyncAlt title="Fetch database entries" />}
-          onClick={() => {
-            setFieldValue('queryType', queryTypes.retrieval);
-            setFieldValue('retrievalOptions.action', retrievalActions.fetch);
-            submitForm();
-          }}
+      <div className="search-and-button-container">
+        <Input
+          type="string"
+          defaultValue=""
+          onChange={(value: string) => setSearchPattern(value)}
+          placeholder="Search..."
         />
-        <Button
-          child={<FaTrashAlt title="Delete all database entries" />}
-          onClick={() => {
-            setShowDeleteAllModal(true);
-          }}
-        />
+        <div className="button-container">
+          <Button
+            child={<FaSyncAlt title="Fetch database entries" />}
+            onClick={() => {
+              setFieldValue('queryType', queryTypes.retrieval);
+              setFieldValue('retrievalOptions.action', retrievalActions.fetch);
+              submitForm();
+            }}
+          />
+          <Button
+            child={<FaTrashAlt title="Delete all database entries" />}
+            onClick={() => {
+              setShowDeleteAllModal(true);
+            }}
+          />
+        </div>
       </div>
       {showDeleteAllModal && (
         <CustomModal
           show={showDeleteAllModal}
           title="Delete all result database entries?"
-          onClose={() => {
-            setShowDeleteAllModal(false);
-          }}
+          onClose={() => setShowDeleteAllModal(false)}
           footer={
-            <Button
-              child="Confirm"
-              onClick={() => {
-                setFieldValue('queryType', queryTypes.retrieval);
-                setFieldValue(
-                  'retrievalOptions.action',
-                  retrievalActions.deleteAll,
-                );
-                submitForm();
-                setShowDeleteAllModal(false);
-              }}
-              className="footer-button"
-            />
+            <div>
+              <Button
+                child="Cancel"
+                onClick={() => setShowDeleteAllModal(false)}
+                className="footer-button"
+              />
+              <Button
+                child="Confirm"
+                onClick={() => {
+                  setFieldValue('queryType', queryTypes.retrieval);
+                  setFieldValue(
+                    'retrievalOptions.action',
+                    retrievalActions.deleteAll,
+                  );
+                  submitForm();
+                  setShowDeleteAllModal(false);
+                }}
+                className="footer-button"
+              />
+            </div>
           }
         />
       )}
-      {rows.length > 0 && (
+      {filteredRows.length > 0 && (
         <table>
           <thead>
             <tr>
@@ -134,7 +157,7 @@ function QueryTabRetrieval() {
               <th></th>
             </tr>
           </thead>
-          <tbody>{rows}</tbody>
+          <tbody>{filteredRows}</tbody>
         </table>
       )}
     </div>
