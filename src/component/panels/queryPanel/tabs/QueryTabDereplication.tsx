31aef08e7aed9bf4e0cbe053ea2a3113696ef4da
michaelwenk <michael.wenk@uni-jena.de>
2021-12-08 16:39:14 +0100

    fix: do not re-render when typing in tolerance and maxAvgDev in QueryTabDereplication

--- src/component/panels/queryPanel/tabs/QueryTabDereplication.tsx
+++ src/component/panels/queryPanel/tabs/QueryTabDereplication.tsx
@@ -1,25 +1,56 @@
-import Tolerances from '../../../elements/Tolerances';
 import { useFormikContext } from 'formik';
 import CheckBox from '../../../elements/CheckBox';
 import { QueryOptions } from '../../../../types/QueryOptions';
+import Input from '../../../elements/Input';
+import { useCallback } from 'react';
 
 function QueryTabDereplication() {
   const { values, setFieldValue } = useFormikContext<QueryOptions>();
+
+  const onChangeToleranceHandler = useCallback(
+    (value: string) => {
+      setFieldValue('dereplicationOptions.shiftTolerance', Number(value));
+    },
+    [setFieldValue],
+  );
+
+  const onChangeMaxAvgDevHandler = useCallback(
+    (value: string) => {
+      setFieldValue('dereplicationOptions.maxAverageDeviation', Number(value));
+    },
+    [setFieldValue],
+  );
+
   return (
     <div>
-      <Tolerances
-        tolerance={values.dereplicationOptions.shiftTolerance}
-        onChangeTolerance={(tolerance: number) =>
-          setFieldValue('dereplicationOptions.shiftTolerance', tolerance)
-        }
-        maxAverageDeviation={values.dereplicationOptions.maxAverageDeviation}
-        onChangeMaxAverageDeviation={(maxAverageDeviation: number) =>
-          setFieldValue(
-            'dereplicationOptions.maxAverageDeviation',
-            maxAverageDeviation,
-          )
-        }
-      />
+      <table>
+        <thead>
+          <tr>
+            <th>Tolerance [ppm]</th>
+            <th>Max. AvgDev [ppm]</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <Input
+                type="number"
+                onChange={(value: string) => onChangeToleranceHandler(value)}
+                defaultValue={values.dereplicationOptions.shiftTolerance}
+                inputWidth="120px"
+              />
+            </td>
+            <td>
+              <Input
+                type="number"
+                onChange={(value: string) => onChangeMaxAvgDevHandler(value)}
+                defaultValue={values.dereplicationOptions.maxAverageDeviation}
+                inputWidth="120px"
+              />
+            </td>
+          </tr>
+        </tbody>
+      </table>
 
       <CheckBox
         defaultValue={values.dereplicationOptions.checkMultiplicity}
